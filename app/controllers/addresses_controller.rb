fab275a5d85c88f3c0005b653bd5560913a1b3ae
Pradeep Chandy <pradeepchandy@gmail.com>
2014-05-04 12:23:41 -0500

    v1.0

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,21 +3,32 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
+
+    if params[:address] == ""
     @address = "the corner of Foster and Sheridan"
+    else
+      @address=  params[:address]
+  end
+
     @url_safe_address = URI.encode(@address)
 
      @url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=true"
-     @raw_data = open(@url).read
+
+
+    @raw_data = open(@url).read
      @parsed_data =JSON.parse(@raw_data)
 
-    #puts parsed_data["results"][1]["geometry"]["location"]["lat"]
+ if @parsed_data["status"]=="ZERO_RESULTS"
+@formatted_address = "please enter correct address"
 
-
-     #puts parsed_data.keys
-
-     @latitude = @parsed_data["results"][1]["geometry"]["location"]["lat"]
-     @longitude = @parsed_data["results"][1]["geometry"]["location"]["lng"]
-
+@latitude = "9999999999999999999"
+@longitude ="9999999999999999999"
+ else
+   @latitude = @parsed_data["results"][0]["geometry"]["location"]["lat"]
+   @longitude = @parsed_data["results"][0]["geometry"]["location"]["lng"]
+   @formatted_address = @parsed_data["results"][0]["formatted_address"]
+end
 
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 AddressToCoords::Application.routes.draw do
   get("/coords", { :controller => "addresses", :action => "fetch_coordinates" })
+  get("/coords/:address", { :controller => "addresses", :action => "fetch_coordinates" })
+
+
 end
